28c197fc4fb482ba24255c6a0d3336d1e5e2faf4
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-22 19:06:41 +0300

    Commit

---
+++ JavaAdvanced/SetsAndMapsAdvanced/Lab/SoftUniParty_02.java
@@ -0,0 +1,37 @@
+package JavaAdvanced.SetsAndMapsAdvanced.Lab;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SoftUniParty_02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Set<String> personsListSet = new TreeSet<>();
+
+        String command = scanner.nextLine();
+        boolean partyOn = false;
+
+        while (!command.equals("END")) {
+            if (command.equals("PARTY")) {
+                command = scanner.nextLine();
+                partyOn = true;
+                continue;
+            } else if (command.length() != 8) {
+                command = scanner.nextLine();
+                continue;
+            }
+            if (partyOn) {
+                personsListSet.remove(command);
+            } else {
+                personsListSet.add(command);
+            }
+            command = scanner.nextLine();
+        }
+        System.out.println(personsListSet.size());
+        for (String item : personsListSet) {
+            System.out.println(item);
+        }
+    }
+}
