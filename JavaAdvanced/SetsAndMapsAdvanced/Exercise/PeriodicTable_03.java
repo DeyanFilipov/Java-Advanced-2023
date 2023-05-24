951542ede67a5a5c7f19c0cb85dfd8769fe0b29d
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-25 00:17:59 +0300

    Commit

---
+++ JavaAdvanced/SetsAndMapsAdvanced/Exercise/PeriodicTable_03.java
@@ -0,0 +1,20 @@
+package JavaAdvanced.SetsAndMapsAdvanced.Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class PeriodicTable_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int rotation = Integer.parseInt(scanner.nextLine());
+        TreeSet<String> periodicTableSet = new TreeSet<>();
+
+        for (int i = 0; i < rotation; i++) {
+            String[] currentElement = scanner.nextLine().split("\\s+");
+            periodicTableSet.addAll(Arrays.asList(currentElement));
+        }
+        periodicTableSet.forEach(item -> System.out.print(item + " "));
+    }
+}
