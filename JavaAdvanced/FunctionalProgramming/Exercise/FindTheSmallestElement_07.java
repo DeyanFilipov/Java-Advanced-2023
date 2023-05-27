ddd2d09150b7c8f0cf35015a5ce9952e4f147ce4
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-27 23:00:57 +0300

    Commit

---
+++ JavaAdvanced/FunctionalProgramming/Exercise/FindTheSmallestElement_07.java
@@ -0,0 +1,21 @@
+package JavaAdvanced.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindTheSmallestElement_07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        int minNumber = numbers.stream().mapToInt(e -> e).min().getAsInt();
+        Function<List<Integer>, Integer> findIndex = item -> {
+            return item.lastIndexOf(minNumber);
+        };
+        System.out.println(findIndex.apply(numbers));
+    }
+}
