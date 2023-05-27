c3d89088d59713a6cd6420a469c909144de797bb
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-27 21:55:00 +0300

    Commit

---
+++ JavaAdvanced/FunctionalProgramming/Exercise/CustomMinFunction_03.java
@@ -0,0 +1,16 @@
+package JavaAdvanced.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class CustomMinFunction_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        Function<int[], Integer> function = arr -> Arrays.stream(arr).min().getAsInt();
+
+        System.out.println(function.apply(numbers));
+    }
+}
