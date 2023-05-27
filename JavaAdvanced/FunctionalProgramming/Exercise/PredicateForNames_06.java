82c316e926e4145ca0435a20a705cd6cfb319b99
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-27 22:49:22 +0300

    Commit

---
+++ JavaAdvanced/FunctionalProgramming/Exercise/PredicateForNames_06.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.FunctionalProgramming.Exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PredicateForNames_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int maxLength = Integer.parseInt(scanner.nextLine());
+        Arrays.stream(scanner.nextLine().split("\\s+")).filter(e -> e.length() <= maxLength).forEach(System.out::println);
+    }
+}
