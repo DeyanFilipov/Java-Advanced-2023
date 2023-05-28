40814b917d15cfe175214eb8223786678e43546b
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-28 13:59:16 +0300

    Commit

---
+++ JavaAdvanced/DefiningClasses/Lab/BankAccount_03/BankAccount.java
@@ -0,0 +1,26 @@
+package JavaAdvanced.DefiningClasses.Lab.BankAccount_03;
+
+public class BankAccount {
+    private static int nextID = 1;
+    private static double interestRate = 0.02;
+
+    private int id;
+    private double balance;
+
+    public BankAccount() {
+        this.id = nextID;
+        nextID++;
+    }
+    public static void setInterestRate(double interestRate) {
+        BankAccount.interestRate = interestRate;
+    }
+    public double getInterestRate(int years) {
+        return balance * years * interestRate;
+    }
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    public int getId() {
+        return id;
+    }
+}
