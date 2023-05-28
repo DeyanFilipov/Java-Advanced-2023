3ede00d87246b32f7012f8ef974ee3341fe7fbfc
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-28 12:48:35 +0300

    Commit

---
+++ JavaAdvanced/DefiningClasses/Lab/CarInfo_01/Car.java
@@ -0,0 +1,34 @@
+package JavaAdvanced.DefiningClasses.Lab.CarInfo_01;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int horsePower;
+
+    public String getBrand() {
+        return this.brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return this.model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getHorsePower() {
+        return this.horsePower;
+    }
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+    public Car() {
+
+    }
+    public Car(String brand, String model, int horsePower) {
+        this.brand = brand;
+        this.model = model;
+        this.horsePower = horsePower;
+    }
+}
---
+++ JavaAdvanced/DefiningClasses/Lab/CarInfo_01/Main.java
@@ -0,0 +1,23 @@
+package JavaAdvanced.DefiningClasses.Lab.CarInfo_01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Car> carsList = new ArrayList<>();
+
+        int carsAmount = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= carsAmount; i++) {
+            String[] inputDataCar = scanner.nextLine().split("\\s+");
+            Car currentCar = new Car(inputDataCar[0], inputDataCar[1], Integer.parseInt(inputDataCar[2]));
+            carsList.add(currentCar);
+
+        }
+        carsList.forEach(item -> System.out.printf("The car is: %s %s - %d HP.\n", item.getBrand(), item.getModel(), item.getHorsePower()));
+    }
+}
