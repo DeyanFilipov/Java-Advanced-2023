26e57b5e5db86bd95937424db90ad882c43fca40
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-05-30 23:16:19 +0300

    Commit

---
+++ JavaAdvanced/DefiningClasses/Exercise/CatLady_09/Siamese.java
@@ -0,0 +1,28 @@
+package JavaAdvanced.DefiningClasses.Exercise.CatLady_09;
+
+public class Siamese extends Cat{
+    private double earSize;
+    private String type = "Siamese";
+
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public Siamese(String name, double earSize) {
+        super(name);
+        this.earSize = earSize;
+    }
+    public double getEarSize() {
+        return earSize;
+    }
+
+    public void setEarSize(double earSize) {
+        this.earSize = earSize;
+    }
+    @Override
+    public String toString() {
+        return String.format("Siamese %s %.2f\n", getName(), getEarSize());
+    }
+}
---
+++ JavaAdvanced/DefiningClasses/Exercise/CatLady_09/StreetExtraordinaire.java
@@ -0,0 +1,32 @@
+package JavaAdvanced.DefiningClasses.Exercise.CatLady_09;
+
+public class StreetExtraordinaire extends Cat {
+    private double meowDecibels;
+    private String type = "StreetExtraordinaire";
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public StreetExtraordinaire(String name, double meowDecibels) {
+        super(name);
+        this.meowDecibels = meowDecibels;
+    }
+
+    public double getMeowDecibels() {
+        return meowDecibels;
+    }
+
+    public void setMeowDecibels(int meowDecibels) {
+        this.meowDecibels = meowDecibels;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("StreetExtraordinaire %s %.2f%n", getName(), getMeowDecibels());
+    }
+}
