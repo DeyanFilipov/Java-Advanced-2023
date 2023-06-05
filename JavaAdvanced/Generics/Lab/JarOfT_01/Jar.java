f23dabf30633fa3f4245ff083b1ffe892f6bcea9
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-05 22:47:25 +0300

    Commit

---
+++ JavaAdvanced/Generics/Lab/JarOfT_01/Jar.java
@@ -0,0 +1,17 @@
+package JavaAdvanced.Generics.Lab.JarOfT_01;
+
+import java.util.ArrayDeque;
+
+public class Jar<T> {
+    private ArrayDeque<T> stack;
+
+    public Jar() {
+        this.stack = new ArrayDeque<>();
+    }
+    public void add(T element) {
+        this.stack.push(element);
+    }
+    public T remove() {
+        return this.stack.pop();
+    }
+}
