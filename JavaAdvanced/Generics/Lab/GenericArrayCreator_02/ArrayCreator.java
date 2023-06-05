de49fb5b6dbbc4774ee193d475aad9b943831d0f
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-05 23:02:49 +0300

    Commit

---
+++ JavaAdvanced/Generics/Lab/GenericArrayCreator_02/ArrayCreator.java
@@ -0,0 +1,20 @@
+package JavaAdvanced.Generics.Lab.GenericArrayCreator_02;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class ArrayCreator {
+    public static  <T> T[] create(int length, T defaultValue) {
+        Class<?> clazz = defaultValue.getClass();
+        T[] arr = (T[])Array.newInstance(clazz, length);
+
+        Arrays.fill(arr, defaultValue);
+        return arr;
+    }
+    public static <T> T[] create(Class<T> clazz, int length, T defaultValue) {
+        T[] arr = (T[]) Array.newInstance(clazz, length);
+
+        Arrays.fill(arr, defaultValue);
+        return arr;
+    }
+}
