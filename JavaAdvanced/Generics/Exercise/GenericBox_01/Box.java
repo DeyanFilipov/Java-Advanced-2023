4b89e87ba861645026b3a3230e5a8eb2363e3fa5
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-07 20:42:57 +0300

    Commit

---
+++ JavaAdvanced/Generics/Exercise/GenericBox_01/Box.java
@@ -0,0 +1,13 @@
+package JavaAdvanced.Generics.Exercise.GenericBox_01;
+
+public class Box<T> {
+    private T data;
+
+    public Box(T data) {
+        this.data = data;
+    }
+    @Override
+    public String toString() {
+        return String.format("%s: %s", this.data.getClass().getName(), this.data);
+    }
+    }
