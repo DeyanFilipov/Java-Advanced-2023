d0ffa3f8c10cba11818b766b86258af9add98d9d
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-07 23:43:44 +0300

    Commit

---
+++ JavaAdvanced/Generics/Exercise/GenericSwapMethodInteger_04/Main.java
@@ -0,0 +1,33 @@
+package JavaAdvanced.Generics.Exercise.GenericSwapMethodInteger_04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        List<Box> boxes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(scanner.nextLine());
+            Box<Integer> box = new Box<>(num);
+            boxes.add(box);
+        }
+        String[] tokens = scanner.nextLine().split("\\s+");
+        int firstIndex = Integer.parseInt(tokens[0]);
+        int secondIndex = Integer.parseInt(tokens[1]);
+
+        swap(boxes, firstIndex, secondIndex);
+        boxes.forEach(System.out::println);
+    }
+
+    private static <T> void swap(List<T> data, int firstIndex, int secondIndex) {
+        T firstElement = data.get(firstIndex);
+        T secondElement = data.get(secondIndex);
+
+        data.set(firstIndex, secondElement);
+        data.set(secondIndex, firstElement);
+    }
+}
