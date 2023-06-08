f9f7c6731a5476def27cba3ecac221295eaf0a85
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-08 22:37:10 +0300

    Commit

---
+++ JavaAdvanced/Generics/Exercise/CustomListIterator_09/Main.java
@@ -0,0 +1,51 @@
+package JavaAdvanced.Generics.Exercise.CustomListIterator_09;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        CustomList<String> customList = new CustomList<>();
+
+        while (!input.equals("END")) {
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "Add":
+                    customList.add(tokens[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(tokens[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    break;
+                case "Greater":
+                    int countGreaterThan = customList.countGreaterThan(tokens[1]);
+                    System.out.println(countGreaterThan);
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Sort":
+                    Sorter.sort(customList);
+                    break;
+                case "Print":
+                    for (String s : customList) {
+                        System.out.println(s);
+                    }
+                    break;
+            }
+            input = scanner.nextLine();
+        }
+    }
+}
