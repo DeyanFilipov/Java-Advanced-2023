d2bbae6769bf625365d5d3308a52e5885b6470f6
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-06-08 21:47:41 +0300

    Commit

---
+++ JavaAdvanced/Generics/Exercise/CustomList_07/Main.java
@@ -0,0 +1,48 @@
+package JavaAdvanced.Generics.Exercise.CustomList_07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        CustomList<String> customList = new CustomList<>();
+
+        while (!input.equals("END")) {
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "Add":
+                    customList.add(tokens[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(tokens[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    break;
+                case "Greater":
+                    int countGreaterThan = customList.countGreaterThan(tokens[1]);
+                    System.out.println(countGreaterThan);
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    customList.forEach(System.out::println);
+                    break;
+            }
+
+
+            input = scanner.nextLine();
+        }
+    }
+}
