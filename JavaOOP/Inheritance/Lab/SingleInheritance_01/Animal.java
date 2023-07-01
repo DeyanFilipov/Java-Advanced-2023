e2f9e46a8000f050e9ced9d9aa1470124b102111
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-07-01 21:00:15 +0300

    Commit

---
+++ JavaOOP/Inheritance/Lab/SingleInheritance_01/Animal.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Lab.SingleInheritance_01;
+
+public class Animal {
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ JavaOOP/Inheritance/Lab/SingleInheritance_01/Main.java
@@ -0,0 +1,10 @@
+package JavaOOP.Inheritance.Lab.SingleInheritance_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
