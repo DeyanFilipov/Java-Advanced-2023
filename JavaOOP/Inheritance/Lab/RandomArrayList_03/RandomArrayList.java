bd1dad87b008719804a2a0b65b9064183d4cd7e6
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-07-01 21:32:35 +0300

    Commit

---
+++ JavaOOP/Inheritance/Lab/RandomArrayList_03/RandomArrayList.java
@@ -0,0 +1,15 @@
+package JavaOOP.Inheritance.Lab.RandomArrayList_03;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomArrayList<T> extends ArrayList<T> {
+
+    private final static Random RANDOM = ThreadLocalRandom.current();
+
+    public T getRandomElement() {
+        int randomIndex = RANDOM.nextInt(super.size());
+        return super.get(randomIndex);
+    }
+}
