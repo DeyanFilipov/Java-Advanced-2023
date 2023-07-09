fcdea26a43bdedcf972c975254b8b43aadd92517
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-07-09 18:04:25 +0300

    Commit

---
+++ JavaOOP/Polymorphism/Exercise/Word_03/CommandInterface.java
@@ -0,0 +1,6 @@
+package JavaOOP.Polymorphism.Exercise.Word_03;
+
+public interface CommandInterface {
+    void init();
+    void handleInput(String input);
+}
---
+++ JavaOOP/Polymorphism/Exercise/Word_03/TextTransform.java
@@ -0,0 +1,5 @@
+package JavaOOP.Polymorphism.Exercise.Word_03;
+
+public interface TextTransform {
+    void invokeOn(StringBuilder text, int startIndex, int endIndex);
+}
