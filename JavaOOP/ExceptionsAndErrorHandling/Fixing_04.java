63d11df2714ecea23d839442c1960c870f9605ff
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-07-18 00:37:21 +0300

    Commit

---
+++ JavaOOP/ExceptionsAndErrorHandling/Fixing_04.java
@@ -0,0 +1,26 @@
+package JavaOOP.ExceptionsAndErrorHandling;
+
+import java.util.Scanner;
+
+public class Fixing_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] weekdays = new String[5];
+
+        weekdays[0] = "Monday";
+        weekdays[1] = "Tuesday";
+        weekdays[2] = "Wednesday";
+        weekdays[3] = "Thursday";
+        weekdays[4] = "Friday";
+
+        for (int i = 0; i < weekdays.length; i++) {
+            try {
+                System.out.println(weekdays[i]);
+            } catch (Exception e) {
+                System.out.printf("Index %s is not valid!", i);
+            }
+        }
+        scanner.nextLine();
+    }
+}
